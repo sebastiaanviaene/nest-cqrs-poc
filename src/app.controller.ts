7f19b6a70a4cd0022ae090760f08aa0ccfe63afe
https://gitlab.com/sebviaene <sebastiaan.viaene@panenco.com>
2024-02-16 18:04:55 +0100

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,6 @@ import { BaseController } from './utils/controller.base';
 export class AppController extends BaseController {
   @Post('/hello')
   getHello(@Body() helloBody: HelloBody) {
-    const body = new HelloBody();
-    body.name = 'Seba';
     return this.execute(helloBody);
   }
 }
